cf712b84b1d47c74efd3c8f6399a870ad9ffc20f
Raghav Dev <rdevm23@users.noreply.github.com>
2024-06-22 18:19:07 +0530

    Delete AVG_RANKS_SUB_INDP.m

--- AVG_RANKS_SUB_INDP.m
+++
@@ -1,9 +0,0 @@
-
-function ranks_of_sub = AVG_RANKS_SUB_INDP(all_ranks,n_ch,n_sub)
-ranks_of_sub = zeros(n_ch,1);
-for s = 1:n_sub
-    ranks = all_ranks(s).refs(2).ranks;
-    ranks_of_sub = ranks_of_sub + ranks;
-end
-ranks_of_sub = ranks_of_sub/n_sub;
-end
